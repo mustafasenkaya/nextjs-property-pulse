b3647f33f10dc40f46fa2d3f5302bce1633dc0ae
mustafasenkaya <musenkaya@gmail.com>
2024-02-23 02:56:05 +0300

    first next.js commit

---
+++ app/layout.js
@@ -0,0 +1,22 @@
+// rafce
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+  title: "PropertyPulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
+// children -> page.js
+// layout -> globals.css
