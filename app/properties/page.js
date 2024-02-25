a865fbdc4a2b7ae609b7dfa2a5ea69614628e633
mustafasenkaya <musenkaya@gmail.com>
2024-02-25 16:19:09 +0300

    Properties Page has been added..

--- app/properties/page.js
+++ app/properties/page.js
@@ -1,11 +1,24 @@
-import Link from "next/link";
+import PropertyCard from "@/components/PropertyCard";
+import propeties from "@/properties.json";
 
 const PropertiesPage = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Properties</h1>
-      <Link href="/">Go Home</Link>
-    </div>
+    // <!-- All Listings -->
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {propeties.length === 0 ? (
+          <p> No Property Found </p>
+        ) : (
+          //
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {propeties.map((property) => (
+              // <div> {property.name} </div>
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
 
