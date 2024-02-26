a3d3f23ae64c0b2207a788ad4dd6424bc75fd08a
mustafasenkaya <musenkaya@gmail.com>
2024-02-26 20:57:18 +0300

    mongoDB test

--- app/page.js
+++ app/page.js
@@ -4,6 +4,8 @@ import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
 
 const HomePage = () => {
+  console.log(process.env.MONGODB_URI);
+
   return (
     <>
       <Hero />
