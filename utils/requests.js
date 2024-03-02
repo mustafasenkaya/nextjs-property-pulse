8f72cfd270c264cee86a3538b130ce0d254bb69d
mustafasenkaya <musenkaya@gmail.com>
2024-03-02 03:13:13 +0300

    Fetch Single Property

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// fetch all propeties
 async function fetchProperties() {
   try {
     // Handle the case where the domain is not available
@@ -22,4 +23,26 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+// fetch single propeties
+async function fetchProperty(id) {
+  try {
+    // Handle the case where the domain is not available
+    if (!apiDomain) {
+      return null;
+    }
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+    // const res = await fetch("http://localhost:3000/api/properties");
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const jsonData = await res.json();
+    return jsonData;
+  } catch (error) {
+    // console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
